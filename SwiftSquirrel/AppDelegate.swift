56d6a6f7d5bfdf7ec709625f836628958c0a167b
Tsungyu Yu <ytyubox@gmail.com>
2020-07-11 15:47:38 +0800

    AppDelegate

--- SwiftSquirrel/AppDelegate.swift
+++ SwiftSquirrel/AppDelegate.swift
@@ -6,12 +6,44 @@
 //
 
 import Cocoa
+import InputMethodKit
+
+private let kConnectionName = "Squirrel_1_Connection"
+
+class SquirrelPanel: NSObject {
+	
+}
+protocol SquirrelConfig {
+	
+}
+protocol SquirrelApplicationDelegate {
+	var menu:NSMenu! {get set}
+	var panel:SquirrelPanel! {get set}
+	var updater:Any! {get set}
+
+	var config:SquirrelConfig {get}
+//	var enableNotifications:Bool{get}
+
+	func deploy(sender:Any)
+	func syncUserData(sender:Any)
+	func configure(sender:Any)
+	func openWiki(sender:Any)
+
+	func setupRime()
+	func startRimeWithFullCheck(fullCheck:Bool)
+	func loadSettings()
+	func loadSchemaSpecificSettings(schemaId:String)
+
+//	var problematicLaunchDetected:Bool {get}
+}
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class SquirrelAppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
-
+	@IBOutlet var menu: NSMenu!
+	@IBOutlet var panel: SquirrelPanel!
+	@IBOutlet var updater: Any!
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
@@ -22,5 +54,54 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	}
 
 
+	var config: SquirrelConfig {
+		fatalError()
+	}
+}
+extension SquirrelAppDelegate:SquirrelApplicationDelegate {
+	
+	
+//	var enableNotifications: Bool {
+//		false
+//	}
+	
+	func deploy(sender: Any) {
+		
+	}
+	
+	func syncUserData(sender: Any) {
+		
+	}
+	
+	func configure(sender: Any) {
+		
+	}
+	
+	func openWiki(sender: Any) {
+		
+	}
+	
+	func setupRime() {
+		
+	}
+	
+	func startRimeWithFullCheck(fullCheck: Bool) {
+		
+	}
+	
+	func loadSettings() {
+		
+	}
+	
+	func loadSchemaSpecificSettings(schemaId: String) {
+		
+	}
+	
 }
 
+
+extension NSApplication {
+	var  squirrelAppDelegate:SquirrelApplicationDelegate {
+		delegate as! SquirrelApplicationDelegate
+	}
+}
---
+++ SwiftSquirrel/Headers/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Squirrel
+//
+//  Created by 游宗諭 on 2020/7/11.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+#import <rime_api.h>
