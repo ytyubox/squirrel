6cac0378ee45f0916906eeb84888f315646ac0ec
Tsungyu Yu <ytyubox@gmail.com>
2020-07-11 13:49:02 +0800

    add SwiftSquirrel

---
+++ SwiftSquirrel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftSquirrel
+//
+//  Created by 游宗諭 on 2020/7/11.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
